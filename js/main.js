009d2d2d2d006417c9aba2278240d473ae4a319d
brandonIT <59006787+brandonIT@users.noreply.github.com>
2020-05-13 14:42:41 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -86,6 +86,25 @@ $(window).on('load', function() {
    		animateIn: 'slideInDown',
 	});
 
+   /* Back to Top
+    * ------------------------------------------------------ */
+    /*var ssBackToTop = function() {
+        
+        var pxShow      = 500,
+            goTopButton = $(".go-top")
+
+        // Show or hide the button
+        if ($(window).scrollTop() >= pxShow) goTopButton.addClass('link-is-visible');
+
+        $(window).on('scroll', function() {
+            if ($(window).scrollTop() >= pxShow) {
+                if(!goTopButton.hasClass('link-is-visible')) goTopButton.addClass('link-is-visible')
+            } else {
+                goTopButton.removeClass('link-is-visible')
+            }
+        });
+    };*/
+
 	/*------------------
 		Circle progress
 	--------------------*/
